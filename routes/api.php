8e688704abc212f1f50e0d498633b8b728f7c7c2
Harshana <harshana@konnect.team>
2025-06-10 05:18:51 +0530

    added migration route

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,24 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\RoleController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/hello', function () {
     return response()->json([
         'message' => 'Hello from Laravel on Railway! 🎉'
     ]);
+});
+
+// Route to run migration **without seeder**
+Route::get('/migrate-only', function () {
+    Artisan::call('migrate', ['--force' => true]);
+    return 'Migration done without seeder!';
+});
+
+// Route to run migration **with seeder**
+Route::get('/migrate-with-seeder', function () {
+    Artisan::call('migrate:fresh', ['--seed' => true, '--force' => true]);
+    return 'Migration and seeding completed!';
 });
 
 Route::group(['prefix' => 'auth'], function ($router) {
