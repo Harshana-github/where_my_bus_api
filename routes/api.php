ba5faadd573b93632e84e6b8332bc6b70ff0511b
Harshana <harshana@konnect.team>
2025-06-10 06:32:53 +0530

    test 2

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DepartmentController;
 use App\Http\Controllers\RoleController;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
@@ -70,3 +71,6 @@ Route::get('/test-register', function () {
 Route::get('/users', function () {
     return response()->json(User::all());
 });
+
+Route::get('/departments', [DepartmentController::class, 'index']);
+Route::post('/departments', [DepartmentController::class, 'store']);
