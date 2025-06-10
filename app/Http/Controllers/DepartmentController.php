984d0661ecd95c6d652b93dc9f9e54426f819efc
Harshana <harshana@konnect.team>
2025-06-10 08:18:25 +0530

    s

---
+++ app/Http/Controllers/DepartmentController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Department;
+use Illuminate\Http\Request;
+
+class DepartmentController extends Controller
+{
+    public function index()
+    {
+        return response()->json(Department::all());
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        return "This is Post";
+
+        $request->validate([
+            'name' => 'required|string|max:255',
+        ]);
+
+        $department = Department::create([
+            'name' => $request->name,
+        ]);
+
+        return response()->json($department, 201);
+    }
+
+    public function show(Department $department)
+    {
+        //
+    }
+
+    public function edit(Department $department)
+    {
+        //
+    }
+
+    public function update(Request $request, Department $department)
+    {
+        //
+    }
+
+    public function destroy(Department $department)
+    {
+        //
+    }
+}
